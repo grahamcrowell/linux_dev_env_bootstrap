6765e701084d0c2a8c806b980136f1a05345a5c8
gcrowell <graham.crowell@visiercorp.com>
2017-08-21 20:49:45 -0700

    sync

--- utils/mount_usb.sh
+++ utils/mount_usb.sh
@@ -6,4 +6,17 @@ sudo fdisk -l
 # make dir to map drive to
 sudo mkdir /media/usb
 
+# mount it
 sudo mount --source /dev/sdb1 --target /media/usb
+
+printf "device mounted to: /media/usb\n"
+ls -l /media/usb
+
+
+# unmount it
+sudo umount /dev/sdb1
+
+# folder size
+du -sh /media/usb/Video/
+# copy file
+rsync -ah --progress /media/usb/Video/Pi\ 1998.avi /home/user/Video/
