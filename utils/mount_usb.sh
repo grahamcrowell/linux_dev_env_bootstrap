3470108899bc40c67d88b51dd485e5f9b91b8953
gcrowell <graham.crowell@gmail.com>
2017-11-22 23:46:14 -0800

    sync TEST

--- utils/mount_usb.sh
+++ utils/mount_usb.sh
@@ -3,23 +3,26 @@
 # list disks: get device (eg /dev/sdb1)
 sudo fdisk -l
 
+SOURCE=/dev/sdc
+DEST=/media/usb4
+
 # make dir to map drive to
-sudo mkdir /media/netdrive
+sudo mkdir $DEST
 
 # install helper 
 sudo apt-get install cifs-utils
 
 # mount it
-sudo mount --source /dev/sdb1 --target /media/netdrive
+sudo mount --source $SOURCE --target $DEST
 
-printf "device mounted to: /media/usb\n"
-ls -l /media/usb
+printf "device mounted to: %s\n" $DEST
+ls -l $DEST
 
 
 # unmount it
-sudo umount /dev/sdb1
+# sudo umount /dev/sdb1
 
 # folder size
-du -sh /media/usb/Video/
+# du -sh /media/usb/Video/
 # copy file
-rsync -ah --progress /media/usb/Video/Pi\ 1998.avi /home/user/Video/
+# rsync -ah --progress /media/usb/Video/Pi\ 1998.avi /home/user/Video/
